9949bf70fcd2eeeed38fa00c0a1836f38bd2c4f2
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-04-25 16:54:35 +0530

    Refactor build script to remove tesseract installation
    Removed installation of tesseract-ocr and updated commands.

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-apt-get update
-apt-get install -y tesseract-ocr
-
 pip install -r requirements.txt
-
-python manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
 python manage.py migrate
