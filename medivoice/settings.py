b7327fad83c8f8700aadbd7b0eb2f43699c67c7a
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-04-25 21:44:05 +0530

    Update settings.py

--- medivoice/settings.py
+++ medivoice/settings.py
@@ -58,7 +58,11 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 if DATABASE_URL:
     DATABASES = {
-        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
 else:
     DATABASES = {
